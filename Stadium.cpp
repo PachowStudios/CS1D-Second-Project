a03da86e5c9adbef2f823fcfdf3155580abcdf26
Ryan Shea <ryansworld10@gmail.com>
2015-04-30 18:52:24 -0700

    Defined JSON format

--- Stadium.cpp
+++ Stadium.cpp
@@ -1,7 +1,7 @@
 #include "Stadium.h"
 
-Stadium::Stadium(QString name, QString team, Address address, qlonglong phoneNumber, Date dateOpened)
-	: name(name), team(team), address(address), phoneNumber(phoneNumber), dateOpened(dateOpened)
+Stadium::Stadium(QString name, QString team, int phoneNumber, int capacity, Address address, Date dateOpened)
+	: name(name), team(team), phoneNumber(phoneNumber), capacity(capacity), address(address), dateOpened(dateOpened)
 { }
 
 bool Stadium::LoadFromJson(const QJsonObject &json, bool allowPartial)
@@ -16,16 +16,21 @@ bool Stadium::LoadFromJson(const QJsonObject &json, bool allowPartial)
 	else if (!allowPartial)
 		return false;
 
+	if (json.contains("PhoneNumber"))
+		phoneNumber = json["PhoneNumber"].toInt();
+	else if (!allowPartial)
+		return false;
+
+	if (json.contains("Capacity"))
+		capacity = json["Capacity"].toInt();
+	else if (!allowPartial)
+		return false;
+
 	if (json.contains("Address"))
 	{
 		if (!address.LoadFromJson(json["Address"].toObject()))
 			return false;
 	}
-	else if (!allowPartial)
-		return false;
-
-	if (json.contains("PhoneNumber"))
-		phoneNumber = json["PhoneNumber"].toVariant().toLongLong();
 	else if (!allowPartial)
 		return false;
 
@@ -46,8 +51,9 @@ QJsonObject Stadium::SaveToJson() const
 
 	json["Name"]        = name;
 	json["Team"]        = team;
-	json["Address"]     = address.SaveToJson();
 	json["PhoneNumber"] = phoneNumber;
+	json["Capacity"]    = capacity;
+	json["Address"]     = address.SaveToJson();
 	json["DateOpened"]  = dateOpened.SaveToJson();
 
 	return json;
--- Stadium.h
+++ Stadium.h
@@ -11,16 +11,17 @@ class Stadium
 {
 public:
 	Stadium() = default;
-	Stadium(QString name, QString team, Address address, qlonglong phoneNumber, Date dateOpened);
+	Stadium(QString name, QString team, int phoneNumber, int capacity, Address address, Date dateOpened);
 
 	bool        LoadFromJson(const QJsonObject &json, bool allowPartial = false);
 	QJsonObject SaveToJson() const;
 	
-	QString   name = "";
-	QString   team = "";
-	Address   address;
-	qlonglong phoneNumber = 0;
-	Date      dateOpened;
+	QString name = "";
+	QString team = "";
+	int     phoneNumber = 0;
+	int     capacity = 0;
+	Address address;
+	Date    dateOpened;
 };
 
 #endif
